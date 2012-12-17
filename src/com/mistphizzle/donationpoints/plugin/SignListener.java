01381667c3adbfe4b8690331628dd3342673fe57
Shawn <MistPhizzle@Gmail.com>
2012-12-16 20:57:27 -0500

    More PremiumSigns handling.
    If the player does not have permission, it breaks the sign and gives it
    back to them.
    If the player has permission, but places it on the ground, it breaks the
    sign and gives it back to them.

--- src/com/mistphizzle/donationpoints/plugin/SignListener.java
+++ src/com/mistphizzle/donationpoints/plugin/SignListener.java
@@ -1,31 +1,44 @@
 package com.mistphizzle.donationpoints.plugin;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.block.Sign;
 
 public class SignListener implements Listener {
 
 	public static DonationPoints plugin;
-	
+
 	public SignListener(DonationPoints instance) {
 		plugin = instance;
 	}
-	
+
 	@EventHandler
 	public void onSignChance(SignChangeEvent e) {
 		if (e.isCancelled()) return;
 		if (e.getPlayer() == null) return;
 		Player p = e.getPlayer();
 		String line1 = e.getLine(0);
-		
+		Block block = e.getBlock();
+		Sign s = (Sign) block.getState();
+
+
 		// Permissions
 		if (line1.equalsIgnoreCase("[Premium]") && !p.hasPermission("donationpoints.sign.create")) {
 			e.setCancelled(true);
+			block.breakNaturally();
 			p.sendMessage("§cYou don't have permission to create DonationPoints signs.");
 		} else if (p.hasPermission("donationpoints.sign.create") && line1.equalsIgnoreCase("[Premium]")) {
-			p.sendMessage("§aYou have created a Premium sign.");
-		}
+			if (block.getType() == Material.SIGN_POST) {
+				p.sendMessage("§cDonationPoints signs must be placed on a wall.");
+				block.breakNaturally();
+				e.setCancelled(true);
+			} else {
+				p.sendMessage("§aYou have created a Premium sign.");
+			}
+		} 
 	}
 }
