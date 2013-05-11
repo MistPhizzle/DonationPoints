4e6c7c32bf17252f469488425a839bcf5ccbc69e
MistPhizzle <mistphizzle@gmail.com>
2013-05-11 15:51:13 -0400

    Config Checker
    Now will check the config on startup to see if any fields need to be
    added.

--- src/com/mistphizzle/donationpoints/plugin/DonationPoints.java
+++ src/com/mistphizzle/donationpoints/plugin/DonationPoints.java
@@ -25,7 +25,7 @@ public class DonationPoints extends JavaPlugin {
 	public static DonationPoints instance;
 
 	public static Permission permission = null;
-	
+
 	private boolean setupPermissions() {
 		RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
 		if (permissionProvider != null) {
@@ -61,12 +61,14 @@ public class DonationPoints extends JavaPlugin {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		setupPermissions();
 
 		// Declare FileConfigurations, load.
 		config = new YamlConfiguration();
 		loadYamls();
+
+		configCheck();
 
 		// Events
 		PluginManager pm = getServer().getPluginManager();
@@ -85,7 +87,7 @@ public class DonationPoints extends JavaPlugin {
 		// Other Variables.
 		PlayerListener.SignMessage = config.getString("General.SignMessage");
 		SignListener.SignMessage = config.getString("General.SignMessage");
-		
+
 		//String Variables
 		Commands.Prefix = Methods.colorize(getConfig().getString("messages.Prefix"));
 		Commands.InvalidArguments = Methods.colorize(getConfig().getString("messages.InvalidArguments"));
@@ -132,7 +134,7 @@ public class DonationPoints extends JavaPlugin {
 		} catch (IOException e) {
 			// Failed to submit stats.
 		}
-
+		
 		if (getConfig().getBoolean("General.ExpireOnStartup")) {
 			ResultSet rs2 = DBConnection.sql.readQuery("SELECT * FROM dp_transactions WHERE expired = 'false' AND expiredate = '" + Methods.getCurrentDate() + "';");
 			try {
@@ -210,6 +212,177 @@ public class DonationPoints extends JavaPlugin {
 		reloadConfig();
 	}
 
-
-
+	public void configCheck() {
+		int ConfigVersion = getConfig().getInt("General.ConfigVersion");
+		if (ConfigVersion != 164) {
+			this.log.info("Config is not up to date! Updating.");
+			// General
+			if (!getConfig().contains("General.AutoCreateAccounts")) {
+				getConfig().set("General.AutoCreateAccounts", true);
+			}
+			if (!getConfig().contains("General.SignMessage")) {
+				getConfig().set("General.SignMessage", "Premium");
+			}
+			if (!getConfig().contains("General.Transferrable")) {
+				getConfig().set("General.Transferrable", true);
+			}
+			if (!getConfig().contains("General.ExpireOnStartup")) {
+				getConfig().set("General.ExpireOnStartup", true);
+			}
+			if (!getConfig().contains("General.AutoFillSigns")) {
+				getConfig().set("General.AutoFillSigns", true);
+			}
+			if (!getConfig().contains("General.SpecificPermissions")) {
+				getConfig().set("General.SpecificPermissions", false);
+			}
+			// MySQL Stuff
+			if (!getConfig().contains("MySQL.engine")) {
+				getConfig().set("MySQL.engine", "sqlite");
+			}
+			if (!getConfig().contains("MySQL.SQLiteDB")) {
+				getConfig().set("MySQL.MySQLiteDB", "donationpoints.db");
+			}
+			if (!getConfig().contains("MySQL.host")) {
+				getConfig().set("MySQL.host", "localhost");
+			}
+			if (!getConfig().contains("MySQL.port")) {
+				getConfig().set("MySQL.port", 3306);
+			}
+			if (!getConfig().contains("MySQL.username")) {
+				getConfig().set("MySQL.username", "root");
+			}
+			if (!getConfig().contains("MySQL.password")) {
+				getConfig().set("MySQL.password", "");
+			}
+			if (!getConfig().contains("MySQL.database")) {
+				getConfig().set("MySQL.database", "minecraft");
+			}
+			// Messages
+			if (!getConfig().contains("messages.Prefix")) {
+				getConfig().set("messages.Prefix", "&7[&aDonationPoints&7] ");
+			}
+			if (!getConfig().contains("messages.NoPermission")) {
+				getConfig().set("messages.NoPermission", "&cYou dont have permission to do that.");
+				}
+			if (!getConfig().contains("messages.InvalidArguments")) {
+				getConfig().set("messages.InvalidArguments", "&cYou have specified an incorrect number of arguments.");
+			}
+			if (!getConfig().contains("messages.PlayerOnly")) {
+				getConfig().set("messages.PlayerOnly", "&cThis command is only available to players.");
+			}
+			if (!getConfig().contains("messages.ReloadSuccessful")) {
+				getConfig().set("messages.ReloadSuccessful", "&cConfig file has been successfully reloaded.");
+			}
+			if (!getConfig().contains("messages.NoCommandExists")) {
+				getConfig().set("messages.NoCommandExists", "&cThat is not a valid DonationPoints command.");
+			}
+			if (!getConfig().contains("messages.DPConfirm")) {
+				getConfig().set("messages.DPConfirm", "&cType &3/dp confirm &cto confirm your purchase of &3&pack &cfor &3%amount points&c.");
+			}
+			if (!getConfig().contains("messages.DPActivate")) {
+				getConfig().set("messages.DPActivate", "&cType &3/dp activate %pack &cto activate your purchase.");
+			}
+			if (!getConfig().contains("messages.DPSuccessfulActivation")) {
+				getConfig().set("messages.DPSuccessfulActivation", "&cYou have activated your &3%pack &cpackage.");
+			}
+			if (!getConfig().contains("messages.DPFailedActivation")) {
+				getConfig().set("messages.DPFailedActivation", "&cYou do not have a &3%pack &cto activate.");
+			}
+			if (!getConfig().contains("messages.ExpireDate")) {
+				getConfig().set("messages.ExpireDate", "&cYour &3%pack &cis set to expire on &3%expiredate&c.");
+			}
+			if (!getConfig().contains("messages.DPGive")) {
+				getConfig().set("messages.DPGive", "&3%amount &chas been added to &3%player &cbalance");
+			}
+			if (!getConfig().contains("messages.DPTake")) {
+				getConfig().set("messages.DPTake", "&3%amount &chas been removed from &3player &c balance.");
+			}
+			if (!getConfig().contains("messages.DPSet")) {
+				getConfig().set("messages.DPSet", "&3%player &caccount has been set to &3%amount points&c.");
+			}
+			if (!getConfig().contains("messages.NoAccount")) {
+				getConfig().set("messages.NoAccount", "&cNo account found for &3%player&c.");
+			}
+			if (!getConfig().contains("messages.AccountCreated")) {
+				getConfig().set("messages.AccountCreated", "&cAn account has been created for &3%player&c.");
+			}
+			if (!getConfig().contains("messages.AccountAlreadyExists")) {
+				getConfig().set("messages.AccountAlreadyExists", "&cAn account already exists for &3player&c.");
+			}
+			if (!getConfig().contains("messages.NoTransfer")) {
+				getConfig().set("messages.NoTransfer", "&cUnable to make the transfer.");
+			}
+			if (!getConfig().contains("messages.TransferOff")) {
+				getConfig().set("messages.TransferOff", "&cThis server does not have transfers enabled.");
+			}
+			if (!getConfig().contains("messages.TransferSent")) {
+				getConfig().set("messages.TransferSent", "&cYou have sent &3%amount &cto &3%player&c.");
+			}
+			if (!getConfig().contains("messages.TransferReceive")) {
+				getConfig().set("messages.TransferReceive", "&cYou have received &3%amount &cfrom &3%player.");
+			}
+			if (!getConfig().contains("messages.PlayerBalance")) {
+				getConfig().set("messages.PlayerBalance", "&cYou have a balance of &3%amount&c.");
+			}
+			if (!getConfig().contains("messages.OtherBalance")) {
+				getConfig().set("messages.OtherBalance", "&3%player &chas a balance of &3%amount&c.");
+			}
+			if (!getConfig().contains("messages.NoPurchaseStarted")) {
+				getConfig().set("messages.NoPurchaseStarted", "&cYou have not yet started a purchase.");
+			}
+			if (!getConfig().contains("messages.PurchaseSuccessful")) {
+				getConfig().set("messages.PurchaseSuccessful", "&cYou have purchased &3%pack &cfor &3%amount points&c.");
+			}
+			if (!getConfig().contains("messages.NeedActivation")) {
+				getConfig().set("messages.NeedActivation", "&cYou have already purchased &3%pack &cbut it is not active.");
+			}
+			if (!getConfig().contains("messages.LimitReached")) {
+				getConfig().set("messages.LimitReached", "&cYou have already reached the limit of &3%limit &cpurchases of &3%pack&c.");
+			}
+			if (!getConfig().contains("messages.PackageActivated")) {
+				getConfig().set("messages.PackageActivated", "&cYou have successfully activated your &3%pack &cpackage");
+			}
+			if (!getConfig().contains("messages.InvalidPackage")) {
+				getConfig().set("messages.InvalidPackage", "&cUnable to find that package.");
+			}
+			if (!getConfig().contains("messages.NotEnoughPoints")) {
+				getConfig().set("messages.NotEnoughPoints", "&cYou don't have enough points to make that purchase.");
+			}
+			if (!getConfig().contains("packages.ExamplePackage.price")) {
+				getConfig().set("packages.ExamplePackage.Price", 100);
+			}
+			if (!getConfig().contains("packages.ExamplePackage.description")) {
+				getConfig().set("packages.ExamplePackage.description", "This is an example package.");
+			}
+			if (!getConfig().contains("packages.ExamplePackage.haslimit")) {
+				getConfig().set("packages.ExamplePackage.haslimit", false);
+			}
+			if (!getConfig().contains("packages.ExamplePackage.limit")) {
+				getConfig().set("packages.ExamplePackage.limit", 3);
+			}
+			if (!getConfig().contains("packages.ExamplePackage.activateimmediately")) {
+				getConfig().set("packages.ExamplePackage.activateimmediately", true);
+			}
+			if (!getConfig().contains("packages.ExamplePackage.expires")) {
+				getConfig().set("packages.ExamplePackage.expires", false);
+			}
+			if (!getConfig().contains("packages.ExamplePackage.expiretime")) {
+				getConfig().set("packages.ExamplePackage.expiretime", 3);
+			}
+			if (!getConfig().contains("packages.ExamplePackage.commands")) {
+				getConfig().createSection("packages.ExamplePackage.commands");
+				List<String> examplecommands = getConfig().getStringList("packages.ExamplePackage.commands");;
+				examplecommands.add("say %player has purchased the example package.");
+				getConfig().set("packages.ExamplePackage.commands", examplecommands);
+			}
+			if (!getConfig().contains("packages.ExamplePackage.expirecommands")) {
+				getConfig().createSection("packages.ExamplePackage.expirecommands");
+				List<String> exampleexpirecommands = getConfig().getStringList("packages.ExamplePackage.expirecommands");;
+				exampleexpirecommands.add("say %player's example package has expired.");
+				getConfig().set("packages.ExamplePackage.expirecommands", exampleexpirecommands);
+			}
+			getConfig().set("General.ConfigVersion", "164");
+			saveConfig();
+		}
+	}
 }
